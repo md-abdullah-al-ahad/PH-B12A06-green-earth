cb9125d4d7a2f670d01391254aabca1dbc69878c
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-09-06 22:12:15 +0600

    fix all trees button issue

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ const loadCategories = () => {
 
 const displayCategory = (categories) => {
   const categoryContainer = document.getElementById("cat-entries");
-  categoryContainer.innerHTML = "";
   for (let category of categories) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
     <button
       class="hover:bg-[#15803D] hover:text-white cursor-pointer w-[100%] text-left px-4 py-2 rounded-lg inter"
       >
-        ${category.category_name}
+        ${category["category_name"]}
     </button>
     `;
     categoryContainer.append(btnDiv);
